979f6636e1438d75d4afb552f2970cf6c14ae4a6
Luke Brown <me@luke.sx>
2015-09-10 15:29:53 +0100

    Edited Services

--- our-services.php
+++ our-services.php
@@ -57,7 +57,44 @@
 	</div>
 </div>
 
+
 <div class="hbm-service">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6" style="padding-top:100px; padding-bottom: 20px;">
+
+				<b class="hbm-uppercase">Starting from $60/hr</b>
+				<h1>Social Media Advertising</h1>
+				<p>.<br>.<br>.<br>.</p>
+				<a class="btn btn-default fill" href="consulting" role="button">view more</a>
+
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="padding-top:50px;">
+					<div class="col-md-4">
+						<i class="btl bt-phone bt-4x bt-white float-right"></i>
+					</div>
+					<div class="col-md-8">
+						<h3>Step One: Dicuss</h3>
+						<p>.<br>.<br>.<br>.</p>
+					</div>
+				</div>
+				<div class="row" style="padding-bottom:50px;">
+					<div class="col-md-4">
+						<i class="btl bt-olist bt-4x bt-white float-right"></i>
+					</div>
+					<div class="col-md-8">
+						<h3>Step Two: Options</h3>
+						<p>.<br>.<br>.<br>.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="hbm-service-2">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6" style="padding-top:100px; padding-bottom: 20px;">
@@ -92,7 +129,7 @@
 	</div>
 </div>
 
-<div class="hbm-service-2">
+<div class="hbm-welcome">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6" style="padding-top:120px; padding-bottom: 30px;">
@@ -129,6 +166,51 @@
 					<div class="col-md-8">
 						<h3>Step Three: Review</h3>
 						<p>Watch as your sales increase, watch as your public interaction improves resulting in a happy customer and a happy you.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="hbm-service">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6" style="padding-top:120px; padding-bottom: 30px;">
+
+				<b class="hbm-uppercase">Starting from $300/per website</b>
+				<h1>Website Development</h1>
+				<p>From a simple business portfolio to complex billing management systems, we have the knowledge and experience to create you a web solution for your business that works.</p>
+				<a class="btn btn-default fill" href="photography" role="button">view more</a>
+
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-md-4">
+						<i class="btl bt-comments bt-4x bt-white float-right"></i>
+					</div>
+					<div class="col-md-8">
+						<h3>Step One: Dicuss</h3>
+						<p>.<br>.<br>.<br>.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<i class="btl bt-camera bt-4x bt-white float-right"></i>
+					</div>
+					<div class="col-md-8">
+						<h3>Step Two: Shoot</h3>
+						<p>.<br>.<br>.<br>.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<i class="btl bt-edit bt-4x bt-white float-right"></i>
+					</div>
+					<div class="col-md-8">
+						<h3>Step Three: Edit</h3>
+						<p>.<br>.<br>.<br>.</p>
 					</div>
 				</div>
 			</div>
